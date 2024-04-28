0da92f7dc2b5a87be94700178e301dbc5b8cbb06
Christina Näsström <kicki@uolim.se>
2024-04-28 22:01:42 +0200

    uppdatering

--- js/script.js
+++ js/script.js
@@ -38,3 +38,17 @@ $(window).on("load", function () {
 		$(this).addClass("active");
 	});
 });
+
+// function myFunction() {
+// 	var element = document.body;
+// 	element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
+// }
+// function stepFunction(event) {
+// 	debugger;
+// 	var element = document.getElementsByClassName("collapse");
+// 	for (var i = 0; i < element.length; i++) {
+// 		if (element[i] !== event.target.ariaControls) {
+// 			element[i].classList.remove("show");
+// 		}
+// 	}
+// }
